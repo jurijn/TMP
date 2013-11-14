68733e0be77df890b29376b629bdbafdc44d3b41
jrj <jurij.nemanic@gmail.com>
2013-11-14 18:15:00 +0100

    index.php updated with tasks, server echoes

--- index.php
+++ index.php
@@ -4,3 +4,19 @@
 // loada potrebne class-e, config, itd...
 // routing -> loada controler in klice metodo v tem kontrolerju
 
+$server_vars = $_SERVER[];
+
+// echoes all server variables
+
+echo "<br/>Server variables are: <br/>";
+
+foreach($server_vars as $key=>$value){
+
+	echo "<br/>key:$key	-> value:$value";
+
+}
+
+// potrebno implementirat dodatne class-e
+// -> naredit preprost svoj MVC, ki ima 
+// router, controler, view, loader ..ect
+
